a65c06a2bf43563ba8b73064ba2e38710de46bcf
mac <neverwhatlose@gmail.com>
2024-10-03 10:25:49 +0300

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //string path = "/Users/mac/projects/c sharp projects/ConsoleApp1/ConsoleApp1/input.txt";
+            string path = "~/input.txt";
+            string[] read = File.ReadAllLines(path);
+            foreach (string s in read)
+            {
+
+            }
+        }
+    }
+}
