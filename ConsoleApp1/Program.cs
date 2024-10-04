c87ed7ac91443bb0e3a2b6fc270cc6d4e7e651bd
mac <neverwhatlose@gmail.com>
2024-10-04 23:14:34 +0300

    LineToArray, CountComposition

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,59 @@
 ﻿namespace ConsoleApp1
 {
-    public class Program
+    public static class Program
     {
         public static void Main()
         {
-            //string path = "/Users/mac/projects/c sharp projects/ConsoleApp1/ConsoleApp1/input.txt";
-            string path = "~/input.txt";
-            string[] read = File.ReadAllLines(path);
-            foreach (string s in read)
-            {
+            string path = Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().IndexOf("bin")) + "input.txt";
+            ConsoleKeyInfo key;
 
-            }
+            do
+            {
+                string[] file = File.ReadAllLines(path);
+                string checkedLine = "";
+
+                foreach (string line in file)
+                {
+                    checkedLine += string.Join(" ", LineToArray(line)) + " ";
+                }
+
+                Console.WriteLine(checkedLine);
+
+                int[] input = LineToArray(checkedLine);
+
+                for (int i = 0; i < input.Length; i++)
+                {
+
+                }
+
+                double a = 4.0;
+                Console.WriteLine(a/0 + " ");
+
+                Console.WriteLine("\nНажмите Q, чтобы завершить программу или любую другую кнопку для повторения");
+                key = Console.ReadKey();
+            } while (key.Key != ConsoleKey.Q);
         }
+
+        private static int[] LineToArray(string line)
+        {
+            int count = 0;
+            string[] array = line.Split(" ");
+            int[] returnArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (int.TryParse(array[i], out int num))
+                {
+                    returnArray[i] = num;
+                    count += 1;
+                }
+            }
+
+            return returnArray[..count];
+        }
+
+        // private static int CountComposition(int[] array)
+        // {
+        //     return 0;
+        // }
     }
 }
