a3b225b004eb223443cfb0d93ecacdd0695ecb3d
mac <neverwhatlose@gmail.com>
2024-10-05 17:23:12 +0300

    fixed LineToArray, added intermediate logging

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ConsoleApp1
+﻿using System.Xml.Schema;
+
+namespace ConsoleApp1
 {
     public static class Program
     {
@@ -11,25 +13,13 @@
             {
                 Console.WriteLine();
                 // 0 или 1: 0 - ошибки не было, 1 - была
-                int exitCode = 0;
                 string[] file = File.ReadAllLines(path);
-                string checkedLine = "";
 
-                int size = string.Join("", LineToArray(file[0])).Length;
-                foreach (string line in file)
-                {
-                    checkedLine += string.Join("", LineToArray(line));
-                    if (size != string.Join("", LineToArray(line)).Length)
-                    {
-                        exitCode = 1;
-                    }
-                }
+                int[] firstArray = LineToArray(file[0]);
+                int[] secondArray = LineToArray(file[1]);
 
-                int[] input = LineToArray(checkedLine);
-                if (exitCode == 0)
-                {
-                    Console.WriteLine(CountComposition(input));
-                }
+                Console.WriteLine(string.Join(" ", firstArray));
+                Console.WriteLine(string.Join(" ", secondArray));
 
                 Console.WriteLine("\nНажмите Q, чтобы завершить программу или любую другую кнопку для повторения");
                 key = Console.ReadKey();
@@ -52,22 +42,32 @@
         {
             // Отсеивание лишних элементов файла
             string checkedLine = "";
-            foreach (char element in line.ToCharArray())
+            foreach (char element in line)
             {
+                Console.WriteLine("lta: " + element);
                 if (int.TryParse(element.ToString(), out int result))
                 {
+                    Console.WriteLine($"parsed element: {element}");
                     checkedLine += result;
+                    Console.WriteLine($"checkedLine: {checkedLine}");
                 }
             }
 
+            Console.WriteLine("---");
+
             // Подготовка готового массива
-            int[] returnArray = new int[checkedLine.ToCharArray().Length];
+            int[] returnArray = new int[checkedLine.Length];
             char[] array = checkedLine.ToCharArray();
+
+            Console.WriteLine($"line: {checkedLine}\nreturnArray: {string.Join(" ", returnArray)}\narray: {string.Join(" ", array)}");
             for (int i = 0; i < returnArray.Length; i++)
             {
-                returnArray[i] = array[i];
+                Console.WriteLine($"returnArray[i]: {returnArray[i]}\narray[i]: {array[i]}");
+                returnArray[i] = int.Parse(array[i].ToString());
+                Console.WriteLine($"returnArray[i] (changed): {returnArray[i]}");
             }
 
+            Console.WriteLine($"returnArray: {string.Join(" ", returnArray)}");
             return returnArray;
         }
 
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c9135d06d3ab3900c0ded4e90483e2f6c0ea25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f812a3af67727e514b20ce895e46b1fd2146e29c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
